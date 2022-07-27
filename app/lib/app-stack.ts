e8863ec172a48c091aca0895d4c5a0ba0b890907
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-27 10:11:38 +0100

    add dynamoDB construct to stack

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,12 +1,17 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_lambda_nodejs, aws_apigateway } from "aws-cdk-lib";
+import { aws_lambda_nodejs, aws_apigateway, aws_dynamodb } from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    // API-Methods which map to a Lambda
     const methods = ["GET", "POST", "PUT", "DELETE"];
+
+    const table = new aws_dynamodb.Table(this, "TodoTable", {
+      partitionKey: { name: "id", type: aws_dynamodb.AttributeType.STRING },
+    });
     const api = new aws_apigateway.RestApi(this, "todo-api");
     const todos = api.root.addResource("todos");
     methods.forEach((method) => {
