cfbd5ec30f15810a549fa7a0b388533fed0d769e
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-27 09:52:18 +0100

    refactor code and add lamba integration to api endpoints

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,22 +1,20 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { aws_lambda_nodejs, aws_apigateway } from "aws-cdk-lib";
-import { readdirSync } from "fs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const methods = ["GET", "POST", "PUT", "DELETE"];
     const api = new aws_apigateway.RestApi(this, "todo-api");
     const todos = api.root.addResource("todos");
-    ["GET", "POST", "PUT", "DELETE"].forEach((method) => {
-      todos.addMethod(method);
-    });
-    const lambdas = readdirSync("functions");
-    lambdas.forEach((method) => {
-      new aws_lambda_nodejs.NodejsFunction(this, method.slice(0, -3), {
-        entry: `functions/${method}`,
+    methods.forEach((method) => {
+      const lambda = new aws_lambda_nodejs.NodejsFunction(this, method, {
+        entry: `functions/${method}.ts`,
       });
+      const integration = new aws_apigateway.LambdaIntegration(lambda);
+      todos.addMethod(method, integration);
     });
   }
 }
