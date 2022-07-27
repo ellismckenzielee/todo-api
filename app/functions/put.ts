edfa21125efda677a84844ae681b8807ccc6e01e
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-27 14:29:16 +0100

    add POST and PUT lambda code with hardcoded IDs

--- app/functions/put.ts
+++ app/functions/put.ts
@@ -1,3 +1,18 @@
-exports.handler = () => {
+import { DynamoDB } from "aws-sdk";
+exports.handler = async () => {
   console.log("inside put function");
+  const docClient = new DynamoDB.DocumentClient();
+
+  try {
+    const data = await docClient
+      .put({
+        TableName: process.env.TABLE_NAME || "",
+        Item: { id: "3", username: "calippo" },
+      })
+      .promise();
+
+    return data;
+  } catch (err) {
+    console.log("ERROR", err);
+  }
 };
