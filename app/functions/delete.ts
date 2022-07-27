2d997cee635adf9886f0ce58902e0fc80bf9b7da
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-27 14:16:11 +0100

    add delete lambda content with hardcoded primary key

--- app/functions/delete.ts
+++ app/functions/delete.ts
@@ -1,3 +1,15 @@
-exports.handler = () => {
+import { DynamoDB } from "aws-sdk";
+exports.handler = async () => {
   console.log("inside delete function");
+  const docClient = new DynamoDB.DocumentClient();
+
+  try {
+    const data = await docClient
+      .delete({ TableName: process.env.TABLE_NAME || "", Key: "1" })
+      .promise();
+
+    return data;
+  } catch (err) {
+    console.log("ERROR", err);
+  }
 };
