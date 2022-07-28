52bff3ef807f27bf91c411e7a5b92fd7f9111511
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-28 10:58:15 +0100

    add api key to api-gw

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -13,21 +13,27 @@ export class AppStack extends Stack {
       partitionKey: { name: "id", type: aws_dynamodb.AttributeType.STRING },
     });
     const api = new aws_apigateway.RestApi(this, "todo-api");
-    api.addApiKey("APIKEY", {
-      apiKeyName: "API_KEY",
-      value: process.env.API_KEY,
-    });
+
     const todos = api.root.addResource("todos");
     methods.forEach((method) => {
       const lambda = new aws_lambda_nodejs.NodejsFunction(this, method, {
         entry: `functions/${method}.ts`,
         environment: {
-          TABLE_NAME: process.env.TABLE_NAME || "",
+          TABLE_NAME: TABLE_NAME || "",
         },
       });
       const integration = new aws_apigateway.LambdaIntegration(lambda);
-      todos.addMethod(method, integration);
+      todos.addMethod(method, integration, { apiKeyRequired: true });
       table.grantFullAccess(lambda);
     });
+    const apiPlan = api.addUsagePlan("apiUsagePlan", {
+      name: "Easy",
+    });
+    const key = api.addApiKey("APIKEY", {
+      apiKeyName: "API_KEY",
+      value: process.env.API_KEY,
+    });
+    apiPlan.addApiKey(key);
+    apiPlan.addApiStage({ stage: api.deploymentStage });
   }
 }
